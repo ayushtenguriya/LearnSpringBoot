9bb144812d53f53ef46eafd214887d551a28f943
Ayush <ayushsharma2509@gmail.com>
2025-06-29 06:10:22 +0530

    added interface file

---
+++ ImplementInterface.java
@@ -0,0 +1,7 @@
+public interface ImplementInterface {
+    //TIP To implement this interface, press <shortcut actionId="ImplementMethods"/> or click the <icon src="AllIcons.Actions.ImplementMethods"/> icon in the gutter.
+    //TIP You can also use the <icon src="AllIcons.Actions.ImplementMethods"/> action from the context menu.
+    void method1();
+    void method2();
+    void method3();
+}
